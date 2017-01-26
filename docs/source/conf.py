d885017596fb8443ef17fbe64904ae33e5c2d155
João Pereira <joaopereira@bytenuts.com>
2017-01-26 17:35:51 +0000

    Accents removed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -128,7 +128,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'NetCoreControls.tex', 'NetCoreControls Documentation',
-     'João Pereira', 'manual'),
+     'Joao Pereira', 'manual'),
 ]
 
 
