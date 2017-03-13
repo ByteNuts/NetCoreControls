166823c0d11163f28fd02373b71c4cea8d68f78b
João Pereira <joaopereira@bytenuts.com>
2017-03-13 15:04:11 +0000

    AppVeyour YML update to VS2017

---
+++ BuildVS17.ps1
@@ -0,0 +1,43 @@
+
+#-------------------------------
+# Installation
+#-------------------------------
+
+Write-Host "Downloading latest .NET Core SDK..."
+
+(New-Object System.Net.WebClient).DownloadFile('https://go.microsoft.com/fwlink/?linkid=841686','dotnet-core-sdk.exe')
+# Invoke-WebRequest "https://go.microsoft.com/fwlink/?linkid=841686" -OutFile "dotnet-core-sdk.exe"
+
+Write-Host "Installing .NET Core SDK..."
+
+Invoke-Command -ScriptBlock { ./dotnet-core-sdk.exe /S /v/qn }
+
+Write-Host "Installation succeeded." -ForegroundColor Green
+
+#-------------------------------
+# Update AppVeyor Build version
+#-------------------------------
+
+Write-Host "Parsing project file..."
+
+[xml]$proj = Get-Content -Path "./NetCoreControls/NetCoreControls.csproj"
+
+$versionPrefix = $proj.Project.PropertyGroup.VersionPrefix
+
+Write-Host "Version prefix: $versionPrefix"
+
+$version = "$versionPrefix-$env:APPVEYOR_BUILD_NUMBER"
+
+Write-Host "Updating AppVeyor build version to $version."
+
+Update-AppveyorBuild -Version $version
+
+#-------------------------------
+# Update AppVeyor Build version
+#-------------------------------
+
+Write-Host "Launching build.cmd..."
+
+./build.cmd
+
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
