df0d0d180296ac0d0c69644a9371596f62d7815e
João Pereira <joaopereira@bytenuts.com>
2017-04-11 10:55:36 +0100

    Updated script

--- Build.ps1
+++ Build.ps1
@@ -48,12 +48,12 @@ function Exec
     }
 }
 
+$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 EnsurePsbuildInstalled
-
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 exec { & dotnet restore }
 exec { & dotnet msbuild "/t:Restore" /p:VersionSuffix=$revision /p:Configuration=Release }
