82aef0ddd809728e4a305ed965c43c1e7ea0bd67
João Pereira <joaopereira@bytenuts.com>
2017-04-11 09:32:07 +0100

    Appveyor fix

--- Build.ps1
+++ Build.ps1
@@ -50,11 +50,11 @@ function Exec
 
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
-#EnsurePsbuildInstalled
+EnsurePsbuildInstalled
 
 #exec { & dotnet restore }
 
-#Invoke-MSBuild
+Invoke-MSBuild
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
